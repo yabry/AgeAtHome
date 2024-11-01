b0aec8b98683381aea1a718203a30d5c5fbbb3e0
yabry <yishai@abry.cc>
2024-11-01 11:57:08 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Age at Home</h1>
+      <p>This is your homepage.</p>
+    </div>
+  );
+}
